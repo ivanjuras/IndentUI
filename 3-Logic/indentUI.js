7cbee26859b224410a6ba3d765b835b8ba0b844a
Ivan Juras <ijuras@gmail.com>
2015-08-11 13:19:17 +0200

    Change IndentUI.js comments and code structure

--- 3-Logic/indentUI.js
+++ 3-Logic/indentUI.js
@@ -2,122 +2,93 @@
 
 ;( function( global, $ ) {
 
-<<<<<<< HEAD
-	// ---------- Global variables ---------- //
+    var $global = $( global ),
+        breakpointMedium = 1180;
 
-		var $global = $( global ),
-				breakpointMedium = 1180;
+    var indentUI = {
+        smoothScrolling: function() {
+            var scrollSpeed = 500;
 
+            $('a[href*=#]:not([href=#]):not([href*=#tab])').on( 'click', function(e) {
+                e.preventDefault();
+                $('html, body').animate(
+                    { scrollTop: $(this.hash).offset().top},
+                    scrollSpeed
+                );
+            });
+        }, // Smooth scrolling on click
 
-	// ---------- indentUI object ---------- //
+        scrollTop: function() {
+            var $btnToTop = $( '.ToTopButton' ),
+                fadeInSpeed = 500,
+                fadeOutSpeed = 500,
+                toTopScrollSpeed = 300;
 
-	var indentUI = {
+            $global.scroll( function() {
+                if ( ( $global.scrollTop() > 150 ) && ( $global.width() < breakpointMedium ) ) {
+                    $btnToTop.fadeIn( fadeInSpeed );
+                } else {
+                    $btnToTop.fadeOut( fadeOutSpeed );
+                }
+            }); 
 
-		// Smooth scrolling on click
-		smoothScrolling: function() {
+            $btnToTop.on( 'click', function() {
+                $( 'html, body' ).animate(
+                    { scrollTop: 0 },
+                    toTopScrollSpeed
+                );
+            });
+        }, // ButtonToTop scroll to top on click
 
-			var scrollSpeed = 500;
+        hamburgerButtonClick: function() {
+            var $btnHamburger = $('.HamburgerButton'),
+                $navMenuA = $('.NavMenu'),
+                menuToggleSpeed = 400;
+            
+            $btnHamburger.on( 'click', function() {
+                $navMenuA.toggle( menuToggleSpeed );
+            });
+        }, // HamburgerButton on click
 
-			$('a[href*=#]:not([href=#]):not([href*=#tab])').on( 'click', function(e) {
-				e.preventDefault();
-				$('html, body').animate(
-					{ scrollTop: $(this.hash).offset().top},
-					scrollSpeed
-				);
-			});
+        tabMenuOnClick: function() {
+            var $tabItem = $( '.TabMenu-tabItem' ),
+                $tabItemLink = $( '.TabMenu-tabLink' ),
+                tabContent = '.TabMenu-tabContent',
+                fadeInSpeed = 400,
+                isActiveClass = 'is-active';
 
-		},
+            $tabItemLink.on( 'click', function( e ) {
+                var parentTabItem = $( this ).parent( $tabItem ),
+                    currentAttr = $( this ).attr('href');
 
-		// ButtonToTop scroll to top on click
-		scrollTop: function() {
+                e.preventDefault();
+                $( tabContent + currentAttr ).fadeIn( fadeInSpeed ).siblings().hide();
+                parentTabItem.addClass( isActiveClass ).siblings().removeClass( isActiveClass );
+            });
+        }, // Tab menu on click
 
-			var $btnToTop = $( '.ToTopButton' ),
-					fadeInSpeed = 500,
-					fadeOutSpeed = 500,
-					toTopScrollSpeed = 300;
+        showHideNavMenu: function() {
+            var $navMenuA = $('.NavMenu');
+            
+            $global.on( 'resize', function() {
+                $(this).width() >= breakpointMedium ? $navMenuA.show() : $navMenuA.hide();
+            });
+        } // Show navigation menu on resize
+    }; // IndentUI object
+    
 
-			$global.scroll( function() {
-				if ( ( $global.scrollTop() > 150 ) && ( $global.width() < breakpointMedium ) ) {
-					$btnToTop.fadeIn( fadeInSpeed );
-				} else {
-					$btnToTop.fadeOut( fadeOutSpeed );
-				}
-			});
+    // ----------  Call functions ---------- //
 
-			$btnToTop.on( 'click', function() {
-				$( 'html, body' ).animate(
-					{ scrollTop: 0 },
-					toTopScrollSpeed
-				);
-			});
-
-		},
-
-		// HamburgerButton on click
-		hamburgerButtonClick: function() {
-
-			var $btnHamburger = $('.HamburgerButton'),
-					$navMenuA = $('.NavMenu'),
-					menuToggleSpeed = 400;
-			
-			$btnHamburger.on( 'click', function() {
-				$navMenuA.toggle( menuToggleSpeed );
-			});
-
-		},
-
-		// Tab menu on click
-		tabMenuOnClick: function() {
-
-			var $tabItem = $( '.TabMenu-tabItem' ),
-					$tabItemLink = $( '.TabMenu-tabLink' ),
-					tabContent = '.TabMenu-tabContent',
-					fadeInSpeed = 400,
-					isActiveClass = 'is-active';
-
-			$tabItemLink.on( 'click', function( e ) {
-
-				var parentTabItem = $( this ).parent( $tabItem ),
-						currentAttr = $( this ).attr('href');
-
-				e.preventDefault();
-				$( tabContent + currentAttr ).fadeIn( fadeInSpeed ).siblings().hide();
-				parentTabItem.addClass( isActiveClass ).siblings().removeClass( isActiveClass );
-
-			});
-
-		},
-
-		// Show navigation menu on resize
-		showHideNavMenu: function() {
-
-			var $navMenuA = $('.NavMenu');
-			
-			$global.on( 'resize', function() {
-				if ( $(this).width() >= breakpointMedium ) {
-					$navMenuA.show();
-				} else {
-					$navMenuA.hide();
-				}
-			});
-
-		}
-
-	};
-	
-
-	// ----------  Call functions ---------- //
-
-	indentUI.smoothScrolling();
-	indentUI.scrollTop();
-	indentUI.hamburgerButtonClick();
-	indentUI.showHideNavMenu();
-	indentUI.tabMenuOnClick();
+    indentUI.smoothScrolling();
+    indentUI.scrollTop();
+    indentUI.hamburgerButtonClick();
+    indentUI.showHideNavMenu();
+    indentUI.tabMenuOnClick();
 
 
-	// ---------- Make IndentUI global ---------- //
+    // ---------- Make IndentUI global ---------- //
 
-	// global.indentUI = indentUI;
+    // global.indentUI = indentUI;
 
 }( window, jQuery ));
 

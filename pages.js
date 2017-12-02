d71e58ef566514e342a0249371d7e169fa526a31
Ivan Juras <ijuras@gmail.com>
2017-12-02 13:28:42 +0100

    Change

--- pages.js
+++ pages.js
@@ -5,5 +5,13 @@ module.exports.pages = [
     category: 'Awesome',
     url: '/posts/post/',
     date: 'Sat, 21 Feb 2015 09:13 GMT'
-  }
+  },
+
+  {
+    title: 'This is my first post',
+    description: 'This is the description of my first post',
+    category: 'Awesome',
+    url: '/posts/post/',
+    date: 'Sat, 21 Feb 2015 09:13 GMT'
+  },
 ];
